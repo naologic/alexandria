c86fb1a16b4429cee935df3b96dafc1c061f9461
c3zar <c3zar@protonmail.ch>
2018-09-01 18:19:17 +0300

    return partials

--- projects/naologic/forms/src/lib/nao-form-group.class.ts
+++ projects/naologic/forms/src/lib/nao-form-group.class.ts
@@ -1,9 +1,10 @@
 import {AbstractControl, AbstractControlOptions, FormGroup} from '@angular/forms';
 import {AsyncValidatorFn, ValidatorFn} from '@angular/forms';
-import {isArray, keys, mapValues, isPlainObject} from 'lodash';
+import {isArray, keys, mapValues, isPlainObject, set, get} from 'lodash';
 import {NaoFormStatic} from './nao-form-static.class';
 
-export class NaoFormGroup extends FormGroup {
+export class NaoFormGroup<T> extends FormGroup {
+  private schema;
 
   constructor(controls: {
       [key: string]: AbstractControl;
@@ -12,6 +13,79 @@ export class NaoFormGroup extends FormGroup {
     super(controls, validatorOrOpts, asyncValidator);
   }
 
+  /**
+   * Get value of this shit
+   */
+  public getValue(): T {
+    return super.getRawValue();
+  }
+
+
+  /**
+   * Get object, but only keep the indexes I need
+   */
+  public getValues(...indexes: string[]): Partial<T> {
+    const data = this.getValue();
+
+    if (indexes.length === 0) {
+      return data;
+    } else {
+      // -->Get: get each index
+      let newData = {};
+      // -->Check: if it's an object or array
+      if (isPlainObject(data) || isArray(data)) {
+        indexes.map(i => {
+          // -->Set: data path in new object
+          newData = set(newData, i, get(data, i) || null);
+        });
+        return newData as Partial<T>;
+      } else {
+        return data as T;
+      }
+    }
+  }
+
+
+  /**
+   * Get value of this shit
+   */
+  public disable(opts?: { onlySelf?: boolean; emitEvent?: boolean; }): any {
+    super.disable();
+  }
+
+  /**
+   * Patch the data, deep
+   */
+  public patchDeep(data?: any) {
+    if (data) {
+      super.patchValue(data);
+    }
+  }
+
+  /**
+   * AJV validate
+   */
+  public validate(): boolean {
+    return true;
+  }
+
+  /**
+   * Add JSON schema with AJV
+   * @param schema
+   */
+  public addJSONSchema(schema: any): boolean {
+    this.schema = schema;
+    return true;
+  }
+
+  /**
+   * Add JSON schema with AJV
+   * @param schema
+   */
+  public removeJSONSchema(schema: any): boolean {
+    this.schema = null;
+    return true;
+  }
 
   /**
    * Get all errors from this form
