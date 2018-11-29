c16573dc5f1b42ab614cba76af3d2a1e1379f6b4
AlexandraMoldoveanu <moldoveanu.alexandra@gmail.com>
2018-11-29 13:19:38 +0200

    added markAllAs methods to NaoFormArray

--- projects/naologic/forms/src/lib/nao-form-array.class.ts
+++ projects/naologic/forms/src/lib/nao-form-array.class.ts
@@ -1,6 +1,8 @@
-import {AbstractControl, AbstractControlOptions, FormArray} from '@angular/forms';
-import {AsyncValidatorFn, ValidatorFn} from '@angular/forms';
-import {NaoFormStatic} from './nao-form-static.class';
+import { mapValues } from 'lodash';
+import { AbstractControl, AbstractControlOptions, FormArray } from '@angular/forms';
+import { AsyncValidatorFn, ValidatorFn } from '@angular/forms';
+import  {NaoFormStatic } from './nao-form-static.class';
+import { NaoFormGroup } from '@naologic/forms/public_api';
 
 export class NaoFormArray extends FormArray {
 
@@ -10,6 +12,73 @@ export class NaoFormArray extends FormArray {
     super(controls, validatorOrOpts, asyncValidator);
   }
 
+  // https://github.com/naologic/alexandria/issues/20
+  private markAs( formGroup: NaoFormGroup|NaoFormArray, type: 'touched'|'untouched'|'dirty'|'pristine'|'pending', options? : { onlySelf?:boolean, emitEvent?: boolean } ): void {
+    mapValues(formGroup.controls, (control) => {
+      if ( control instanceof NaoFormGroup || control instanceof NaoFormArray ){
+        this.markAs( control, type, options );
+      }
+      else {
+        switch (type) {
+          case 'touched':
+            control.markAsTouched(options);
+            break;
+          case 'untouched':
+            control.markAsUntouched(options);
+            break;
+          case 'dirty':
+            control.markAsDirty(options);
+            break;
+          case 'pristine':
+            control.markAsPristine(options);
+            break;
+          case 'pending':
+            control.markAsPending(options);
+            break;
+        }
+       }
+    });    
+  }
+
+  /**
+   * Iterates through all the children of the NaoFormGroup, NaoFormArray and calls markAsTouched on all controls;
+   * for NaoFormControl it references the native function markAsTouched  
+  */
+  public markAllAsTouched(opts?: { onlySelf?: boolean }): void {
+    this.markAs( this, 'touched', opts );
+  }
+
+  /**
+   * Iterates through all the children of the NaoFormGroup, NaoFormArray and calls markAsUntouched on all controls
+   * for NaoFormControl it references the native function markAsUntouched  
+  */
+  public markAllAsUntouched(opts?: { onlySelf?: boolean }): void {
+    this.markAs( this, 'untouched', opts );
+  }
+
+  /**
+   * Iterates through all the children of the NaoFormGroup, NaoFormArray and calls markAsDirty on all controls
+   * for NaoFormControl it references the native function markAsDirty
+  */
+  public markAllAsDirty(opts?: { onlySelf?: boolean }): void {
+    this.markAs( this, 'dirty', opts );
+  }
+
+  /**
+   * Iterates through all the children of the NaoFormGroup, NaoFormArray and calls markAsPristine on all controls
+   * for NaoFormControl it references the native function markAsPristine
+  */
+  public markAllAsPristine(opts?: { onlySelf?: boolean }): void {
+    this.markAs( this, 'pristine', opts );
+  }
+
+  /**
+   * Iterates through all the children of the NaoFormGroup, NaoFormArray and calls markAsPending on all controls
+   * for NaoFormControl it references the native function markAsPending
+  */
+  public markAllAsPending(opts?: { onlySelf?: boolean, emitEvent?: boolean }): void {
+    this.markAs( this, 'pending', opts );
+  }
 
   /**
    * Get all errors from this form
